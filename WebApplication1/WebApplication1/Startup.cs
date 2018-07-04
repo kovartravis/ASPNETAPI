783ee76a8f6ce58bc83e99205aae60a690830aba
Travis Kovar <kovartravis@gmail.com>
2018-07-04 17:25:38 -0500

    added db name

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -26,7 +26,7 @@ namespace ExercisesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase());
+            services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("ExercisesDb"));
             services.AddMvc();
         }
 
